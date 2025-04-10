b769cabe780bf8f241308a3e70f9d94f4ae36a32
Diana Moreno <diana.moreno6@gmail.com>
2025-04-09 21:38:22 -0500

    Implement command to check db availability and apply code style fixes

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,9 +2,9 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
 
     def test_add_numbers(self):
-        res = calc.add(3,2)
+        res = calc.add(3, 2)
         self.assertEqual(res, 5)
-         
